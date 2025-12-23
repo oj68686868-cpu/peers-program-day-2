8a272ba04954a70c6165449678bea39c64a327c8
oj68686868-cpu <oj68686868@gmail.com>
2025-12-23 11:55:45 +0530

    Add files via upload

--- Python.py
+++ Python.py
@@ -1,9 +1,8 @@
-# Reverse Words in a String
-def reverseWords(s):
-    words = s.split()
-    result = []
-    for i in range(len(words) - 1, -1, -1):
-        result.append(words[i])
-    return " ".join(result)
-s = input()
-print(reverseWords(s))
+# Check if Array Is Sorted
+def isSorted(nums):
+    for i in range(len(nums) - 2):
+        if nums[i] > nums[i + 1]:
+            return False
+    return True
+nums = list(map(int, input().split()))
+print(isSorted(nums))
